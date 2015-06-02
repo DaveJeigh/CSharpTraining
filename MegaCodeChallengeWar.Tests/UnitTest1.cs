930e9e2b360dc1efe1442eec58199491714ff730
DaveJeigh <david.jellison@gmail.com>
2015-06-02 17:21:22 +0200

    added untracked files

---
+++ MegaCodeChallengeWar.Tests/UnitTest1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MegaCodeChallengeWar;
+
+namespace MegaCodeChallengeWar.Tests
+{
+    [TestClass]
+    public class UnitTestCard
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            PlayingCard playingCard = new PlayingCard(2, PlayingCard.SuiteType.Clubs);
+            Assert.IsNotNull(playingCard);
+        }
+
+        [TestMethod]
+        public void TestCardName()
+        {
+            PlayingCard playingCard = new PlayingCard(2, PlayingCard.SuiteType.Clubs);
+            Assert.AreEqual(playingCard.Name, "Two of Clubs");
+        }
+
+        [TestMethod]
+        public void CardID()
+        {
+            PlayingCard playingCard = new PlayingCard(2, PlayingCard.SuiteType.Clubs);
+            Assert.AreEqual(playingCard.PlayingCardID, 102);
+        }
+
+        [TestMethod]
+        public void CardRank()
+        {
+            PlayingCard playingCard = new PlayingCard(2, PlayingCard.SuiteType.Clubs);
+            Assert.AreEqual(playingCard.Rank, PlayingCard.RankType.Two);
+        }
+
+        [TestMethod]
+        public void CardToString()
+        {
+            PlayingCard playingCard = new PlayingCard(2, PlayingCard.SuiteType.Clubs);
+            Assert.AreEqual(playingCard.ToString(), "Two of Clubs");
+        }
+
+        [TestMethod]
+        public void AceOfDiamonds()
+        {
+            PlayingCard playingCard = new PlayingCard(14, PlayingCard.SuiteType.Diamonds);
+            Assert.AreEqual(playingCard.Name, "Ace of Diamonds");
+            Assert.AreEqual(playingCard.PlayingCardID, 214);
+        }
+
+        [TestMethod]
+        public void KingOfHearts()
+        {
+            PlayingCard playingCard = new PlayingCard(13, PlayingCard.SuiteType.Hearts );
+            Assert.AreEqual(playingCard.Name, "King of Hearts");
+            Assert.AreEqual(playingCard.PlayingCardID, 313);
+        }
+
+        [TestMethod]
+        public void QueenOfSpades()
+        {
+            PlayingCard playingCard = new PlayingCard(12, PlayingCard.SuiteType.Spades );
+            Assert.AreEqual(playingCard.Name, "Queen of Spades");
+            Assert.AreEqual(playingCard.PlayingCardID, 412);
+        }
+
+        [TestMethod]
+        public void TooLowValue()
+        {
+            try
+            {
+                PlayingCard playingCard = new PlayingCard(1, PlayingCard.SuiteType.Spades);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is ArgumentOutOfRangeException);       
+            }
+        }
+
+        [TestMethod]
+        public void TooHighValue()
+        {
+            try
+            {
+                PlayingCard playingCard = new PlayingCard(15, PlayingCard.SuiteType.Spades);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is ArgumentOutOfRangeException);
+            }
+        }
+
+    }
+
+    [TestClass]
+    public class TestDeck
+    {
+        [TestMethod]
+        public void DeckConstructor()
+        {
+            DeckOfCards cardDeck = new DeckOfCards(DeckOfCards.DeckType.Standard);
+            Assert.IsNotNull(cardDeck);
+        }
+
+        [TestMethod]
+        public void DeckConstructorNonStandard()
+        {
+            try
+            {
+                DeckOfCards cardDeck = new DeckOfCards(DeckOfCards.DeckType.StandardPlus2Jokers);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is NotImplementedException);
+            }
+        }
+
+        [TestMethod]
+        public void TestBuildDeck()
+        {
+            DeckOfCards cardDeck = new DeckOfCards(DeckOfCards.DeckType.Standard);
+            cardDeck.BuildDeck();
+            Assert.AreEqual(cardDeck.SetOfCards.Count, 52);
+            Assert.AreEqual(cardDeck.SetOfCards[0].Name, "Two of Clubs");
+            Assert.AreEqual(cardDeck.SetOfCards[1].Name, "Three of Clubs");
+            Assert.AreEqual(cardDeck.SetOfCards[12].Name, "Ace of Clubs");
+            Assert.AreEqual(cardDeck.SetOfCards[13].Name, "Two of Diamonds");
+            Assert.AreEqual(cardDeck.SetOfCards[50].Name, "King of Spades");
+            Assert.AreEqual(cardDeck.SetOfCards[51].Name, "Ace of Spades");
+        }
+
+        [TestMethod]
+        public void TestShuffle()
+        {
+            DeckOfCards cardDeck = new DeckOfCards(DeckOfCards.DeckType.Standard);
+            cardDeck.BuildDeck();
+            cardDeck.ShuffleDeck();
+            // Not a good test of Randomness - since a shuffle COULD produce this
+            //  But the odds are very, very long 
+            //  - and thus a very simple test to make sure a shuffle occurred
+            Assert.IsFalse(
+                (cardDeck.SetOfCards[0].Name == "Two of Clubs") &&
+                (cardDeck.SetOfCards[51].Name == "Ace of Spades") &&
+                (cardDeck.SetOfCards[12].Name == "Ace of Clubs")
+                );
+        }
+
+        [TestMethod]
+        public void TestPerformShuffles()
+        {
+            DeckOfCards cardDeck = new DeckOfCards(DeckOfCards.DeckType.Standard);
+            cardDeck.BuildDeck();
+            cardDeck.PerformShuffles();
+            Assert.IsFalse(
+                (cardDeck.SetOfCards[0].Name == "Two of Clubs") &&
+                (cardDeck.SetOfCards[51].Name == "Ace of Spades") &&
+                (cardDeck.SetOfCards[12].Name == "Ace of Clubs")
+                );
+        }
+
+        [TestMethod]
+        public void TestCutDeck()
+        {
+            DeckOfCards cardDeck = new DeckOfCards(DeckOfCards.DeckType.Standard);
+            cardDeck.BuildDeck();
+            cardDeck.CutDeck();
+        }
+    }
+
+    [TestClass]
+    public class TestHousePlayer
+    {
+        [TestMethod]
+        public void HousePlayerConstructor()
+        {
+            DeckOfCards cardDeck = new DeckOfCards(DeckOfCards.DeckType.Standard);
+            cardDeck.BuildDeck();
+            //cardDeck.PerformShuffles();
+            GamePlayer player1 = new GamePlayer("James");
+            Assert.IsNotNull(player1.HandOfCards);
+
+            GamePlayer player2 = new GamePlayer("Don");
+
+            HousePlayer housePlayer = new HousePlayer(cardDeck, player1, player2);
+            Assert.IsNotNull(housePlayer);
+        }
+
+
+        [TestMethod]
+        public void HousePlayerDeal()
+        {
+            DeckOfCards cardDeck = new DeckOfCards(DeckOfCards.DeckType.Standard);
+            cardDeck.BuildDeck();
+            cardDeck.PerformShuffles();
+            GamePlayer player1 = new GamePlayer("Jimmy");
+            GamePlayer player2 = new GamePlayer("Teresa");
+
+            HousePlayer housePlayer = new HousePlayer(cardDeck, player1, player2);
+            housePlayer.DealCardsForWar();
+
+            Assert.AreEqual(player1.HandOfCards.Count, 26);
+            Assert.AreEqual(player2.HandOfCards.Count, 26);
+        }
+
+
+
+    }
+}
---
+++ MegaCodeChallengeWar/PlayingCard.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MegaCodeChallengeWar
+{
+    public class PlayingCard
+    {
+        public int PlayingCardID { get; set; }
+        public RankType Rank { get; set; }
+        public int RankValue { get; set; }
+        public SuiteType Suite { get; set; }
+        public string Name { get; set; }
+
+        public enum RankType {Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace};
+        public enum SuiteType { Clubs = 1, Diamonds, Hearts, Spades };
+
+        public PlayingCard(int value, SuiteType suite)
+        {
+            if ((value < 2) || (value > 14))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            
+            this.RankValue = value;
+            this.Suite = suite;
+            assignRankAndNameAndID();
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        private void assignRankAndNameAndID()
+        {
+            switch (this.RankValue)
+            {
+                case 2:
+                    this.Rank = RankType.Two;
+                    break;
+                case 3:
+                    this.Rank = RankType.Three;
+                    break;
+                case 4:
+                    this.Rank = RankType.Four;
+                    break;
+                case 5:
+                    this.Rank = RankType.Five;
+                    break;
+                case 6:
+                    this.Rank = RankType.Six;
+                    break;
+                case 7:
+                    this.Rank = RankType.Seven;
+                    break;
+                case 8:
+                    this.Rank = RankType.Eight;
+                    break;
+                case 9:
+                    this.Rank = RankType.Nine;
+                    break;
+                case 10:
+                    this.Rank = RankType.Ten;
+                    break;
+                case 11:
+                    this.Rank = RankType.Jack;
+                    break;
+                case 12:
+                    this.Rank = RankType.Queen;
+                    break;
+                case 13:
+                    this.Rank = RankType.King;
+                    break;
+                case 14:
+                    this.Rank = RankType.Ace;
+                    break;
+            }
+
+            this.Name = this.Rank.ToString() + " of " + this.Suite.ToString();
+            this.PlayingCardID = (100 * (int)(this.Suite)) + this.RankValue;
+        }
+    }
+}
